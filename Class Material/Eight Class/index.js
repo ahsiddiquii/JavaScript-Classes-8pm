ef55b6a8002bb070e3bc5f1873513d11d4eae3a3
ahsiddiquii <abdulhaseeb72427@gmail.com>
2024-09-29 16:22:27 +0500

    Class Seventh Continue Array Methods

---
+++ Class Material/Eight Class/index.js
@@ -0,0 +1,153 @@
+console.log("Running");
+
+// Array remaining methods:
+
+// Push()
+
+// Examples:
+
+// 1
+// let array = [1,2,3,4,5,6,7];
+// let another_Value = 8;
+
+// array.push(another_Value);
+// console.log(array);
+// console.log(another_Value);
+
+// array.push(9);
+// console.log(array);
+
+// 2 
+
+// let array = ["A", "B", "C"];
+// let myName = "Abdul Haseeb Siddiqui"
+
+// array.push(myName);
+// array.push("Mudasir")
+// console.log(array);
+
+
+// pop()
+
+// let student = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// console.log(student);
+// student.pop();
+
+// console.log(student);
+
+
+// 2
+// let student = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// let remove_Last_Value = student.pop();
+// console.log(student);
+// console.log(remove_Last_Value);
+
+
+
+// Shift()
+
+// let array = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// console.log(array);
+// let removed_Value = array.shift();
+// console.log(array);
+// console.log(removed_Value);
+
+
+// unshift()
+
+// let array = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// console.log(array);
+// let add_Value = array.unshift("Hasan");
+// console.log(array);
+// console.log(add_Value); // Return the length of array:
+
+
+// Include();
+
+// let array = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// console.log(array.includes("Mudasir", 1));
+
+
+// Join()
+
+// let array = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// let array_2 = array.join(" / ");
+// console.log(array)
+// console.log(array_2);
+// console.log(typeof array_2);
+
+
+// Split():
+
+// let name = "Abdul/Haseeb/Siddiqui";
+
+// let name_array = name.split("/");
+// console.log(name);
+// console.log(name_array);
+
+// Use Of document.write():
+// let name = "Abdul/Haseeb/Siddiqui";
+// document.write(name)
+
+// Slice();
+
+// Examples: 
+// 1
+// let array = [1,2,3,4,5,6,7,8]
+// let slice_array = array.slice(3, 6);
+// console.log(array);
+// console.log(slice_array);
+// 2
+// let array = [1,2,3,4,5,6,7,8]
+// let slice_array = array.slice();
+// console.log(array);
+// console.log(slice_array);
+// 3
+// let array = [1,2,3,4,5,6,7,8]
+// let slice_array = array.slice(3, 6);
+// console.log(array);
+// console.log(slice_array);
+
+// 4 
+// let students = ["Mudasir", "Danial", "Haseeb", "Syed"];
+// let slice_array = students.slice(1, 3);
+// console.log(slice_array);
+// console.log(students);
+
+// 5
+// let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// let slice_array = array.slice(-5, -2);
+// console.log(array);
+// console.log(slice_array);
+
+
+// Splice():
+// Examples:
+// 1
+// let array = [1,2,3,91,5,6,7,8,9,10,11,12,13,14,15];
+// array.splice(3, 1, 4)
+// console.log(array);
+// 1
+// let array = [1,2,3,91,5,6,7,8,9,10,11,12,13,14,15];
+// array.splice(3, 1)
+// console.log(array);
+
+// indexOf():
+// Examples:
+// 1
+// let fruits = ["Apples", "Mangoes", "Grapes", "Peach", "Oranges", "plums"];
+// let index = fruits.indexOf("Grapes");
+// console.log(index); // Output: 2
+// console.log(fruits[index]); // Grapes
+// console.log(fruits[2]); // Grapes
+
+// 2
+// let array = [1,2,3,91,5,6,7,8,9,10,11,12,13,14,15];
+// let index_Number = array.indexOf(2, 2);
+// console.log(index_Number);
+
+
+
+
+
+
